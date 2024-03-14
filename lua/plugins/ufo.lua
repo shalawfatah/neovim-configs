3a4a076ed9d650e337a7ce57376d67c5e26791aa
Shalaw Fatah <shalaw.fatah@gmail.com>
2024-03-14 13:54:19 -0700

    fixing ufo issue

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -8,12 +8,6 @@ return {
       dynamicRegistration = false,
       lineFoldingOnly = true,
     }
-    local language_servers = require("lspconfig").util.available_servers()
-    for _, ls in ipairs(language_servers) do
-      require("lspconfig")[ls].setup({
-        capabilities = capabilities,
-      })
-    end
     require("ufo").setup({})
     vim.keymap.set("n", "zR", ufo.openAllFolds)
     vim.keymap.set("n", "zM", ufo.closeAllFolds)
