df38b4998a61213310b9e6de1478ef8a52fc68b9
Shalaw Fatah <shalaw.fatah@gmail.com>
2024-03-20 13:52:05 -0700

    startup update

--- lua/plugins/startup.lua
+++ lua/plugins/startup.lua
@@ -24,7 +24,7 @@ return {
       dashboard.button("<leader>w", "󰱼 > Project list", "<cmd>Telescope projects<CR>"),
       dashboard.button("<leader>o", " > File editor", "<CMD>Oil<CR>"),
       dashboard.button("<leader>h", " > Command history", ":Telescope command_history<CR>"),
-      dashboard.button("<leader>.", "󰁯  > Neovim Config", ":cd ~/.config/nvim | edit init.lua<CR>"),
+      dashboard.button("<leader>.", "󰁯 > Neovim Config", ":cd ~/.config/nvim | edit init.lua<CR>"),
       dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
     }
     -- Send config to alpha
