63a187d1bda674bc4455e24fe68ef4415b3e09e7
Shalaw Fatah <shalaw.fatah@gmail.com>
2024-03-22 01:50:55 -0700

    keybindings file regulated

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,58 +1,36 @@
+-- LOCAL VARIABLES
 local key = vim.keymap.set
-
-key("n", "<leader>h", [[:nohl <CR>]], {})
-key("n", "<leader>e", [[:bprevious <CR>]], {})
-key("n", "<leader>n", [[:bnext <CR>]], {})
-
--- JS/TS KEYBINDINGS
-key("n", "<leader>js", [[:!node %<CR>]], { noremap = true, silent = true })
-key("n", "<leader>jj", [[:'<,'>w !node<CR>]], { noremap = true, silent = true })
-key("n", "<leader>ts", [[:!tsc %<CR>]], { noremap = true, silent = true })
-key("n", "<leader><Enter>", [[:!npm run start<CR>]], { noremap = true, silent = true })
-key("n", "<leader>dev", [[:!npm run dev<CR>]], { noremap = true, silent = true })
-key("n", "<leader>bd", [[:!npm run build<CR>]], { noremap = true, silent = true })
-
--- SCREENSHOT KEYBINDINGS
-key("n", "<C-s>", ":Silicon<CR>", { noremap = true })
-key("v", "<C-s>", ":Silicon<CR>", { noremap = true })
-
--- GIT KEYBINDINGS
-key("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-key("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", {})
-
--- EDIT KEYBINDINGS
-key("n", "<leader>o", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-key("v", "<leader>o", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-
--- FILE EXPLORER KEYBINDINGS
-key("n", "<leader>b", ":Neotree filesystem toggle<CR>", { noremap = true, silent = true })
-key("v", "<leader>b", ":Neotree filesystem toggle<CR>", { noremap = true, silent = true })
-
--- LSP KEYBINDINGS
-key("n", "K", vim.lsp.buf.hover, {}) -- hover
-key("n", "gd", vim.lsp.buf.definition, {}) -- where is the code defined
-key("n", "gt", vim.lsp.buf.type_definition, {}) -- what is the type of the code
-key("n", "<leader>ft", vim.lsp.buf.format, {}) -- what is the type of the code
-key("n", "<leader>ca", "<CMD>lua vim.lsp.buf.code_action()<CR>", {}) -- what actions can be done
-key("n", "<leader>rf", ":Telescope lsp_references<CR>", {}) -- where is the code referenced
-key("n", "<leader>rn", "<CMD> lua vim.lsp.buf.rename()<CR>", {}) -- rename the code throughout all
-key("i", "<C-Enter>", "<CMD> lua vim.lsp.buf.completion()<CR>", {}) -- what comes as code completion
-key("n", "<leader>ds", "<CMD> lua vim.lsp.buf.document_symbol()<CR>", {}) -- declared vars, funcs, classes
-
--- MARKDOWN PREVIEW
-key("n", "<leader>md", ":MarkdownPreview<CR>", { noremap = true, silent = true })
-key("n", "<leader>mdc", ":MarkdownPreviewStop<CR>", { noremap = true, silent = true })
-key("n", "<leader>mdt", ":MarkdownPreviewToggle<CR>", { noremap = true, silent = true })
-
--- Live Server
-key("n", "<leader>live", ":LiveServerStart<CR>", { noremap = true, silent = true })
-key("n", "<leader>stop", ":LiveServerStop<CR>", { noremap = true, silent = true })
-
+local nor = { noremap = true, silent = true }
+local buf = ":lua vim.lsp.buf."
 -- TELESCOPE KEYBINDINGS
-key("n", "<C-p>", ":Telescope find_files<CR>", {})
-key("n", "<leader>fg", ":Telescope live_grep<CR>", {})
-key("n", "<leader>fb", ":Telescope buffers<CR>", {})
-key("n", "<leader>fh", ":Telescope help_tags<CR>", {})
-key("n", "<leader>u", ":Telescope undo_tree<CR>", {})
-key("n", "<leader>w", ":Telescope projects<CR>", {})
-key("n", "<leader>y", ":Telescope neoclip<CR>", {})
+key("n", "<C-p>", ":Telescope find_files<CR>", nor)                    -- SHOW FILES IN THE PROJECT
+key("n", "<leader>lg", ":Telescope live_grep<CR>", nor)                -- SEARCH PROJECT FOR WORDS
+key("n", "<leader>bf", ":Telescope buffers<CR>", nor)                  -- SHOW OPEN BUFFERS
+key("n", "<leader>h", ":Telescope help_tags<CR>", nor)                 -- SHOW HELP TAGS
+key("n", "<leader>u", ":Telescope undo<CR>", nor)                      -- SHOW UNDO TREE
+key("n", "<leader>w", ":Telescope projects<CR>", nor)                  -- SHOW PROJECTS
+key("n", "<leader>y", ":Telescope neoclip<CR>", nor)                   -- SHOW COPY-PASTE TREE
+-- LSP KEYBINDINGS
+key("n", "K", buf .. "hover()<CR>", nor)                               -- HOVER
+key("n", "gd", buf .. "definition()<CR>", nor)                         -- WHERE IS THE CODE DEFINED
+key("n", "gt", buf .. "type_definition()<CR>", nor)                    -- WHAT IS THE TYPE OF THE CODE
+key("n", "<leader>ft", buf .. "format() <CR>", nor)                    -- WHAT IS THE TYPE OF THE CODE
+key("n", "<leader>ca", buf .. "code_action()<CR>", nor)                -- WHICH CODE ACTION IS AVAILABLE
+key("n", "<leader>rf", ":Telescope lsp_references<CR>", nor)           -- WHERE IS THE CODE REFERENCED
+key("n", "<leader>rn", buf .. "rename()<CR>", nor)                     -- RENAME THE CODE THROUGHOUT ALL
+key("i", "<C-Enter>", buf .. "completion()<CR>", nor)                  -- WHAT COMES AS CODE COMPLETIOn
+key("n", "<leader>ds", buf .. "document_symbol()<CR>", nor)            -- DECLARED VARS, FUNCS, CLASSES
+-- COMMON LANGUAGE KEYBINDINGS
+key("n", "<leader>js", [[:!node %<CR>]], nor)                          -- RUN CURRENT JS FILE
+key("n", "<leader>ts", [[:!tsc %<CR>]], nor)                           -- RUN CURRENT TS FILE
+key("n", "<leader>go", [[:!go run %<CR>]], nor)                        -- RUN CURRENT GO FILE
+-- Live Server
+key("n", "<leader>live", ":LiveServerStart<CR>", nor)                  -- START LIVE SERVER
+key("n", "<leader>stop", ":LiveServerStop<CR>", nor)                   -- STOP LIVE SERVER
+-- UTILITY KEYS
+key({ "n", "v" }, "<leader>o", ":Oil<CR>", nor)                        -- FILE EDITING
+key({ "n", "v" }, "<C-s>", ":Silicon<CR>", { noremap = true })         -- SCREENSHOT
+key({ "n", "v" }, "<leader>b", ":Neotree filesystem toggle<CR>", nor)  -- EXPLORER
+key("n", "<leader>md", ":MarkdownPreviewToggle<CR>", nor)              -- MARKDOWN TOGGLE
+key("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", nor) -- SHOW GIT LINE COMMIT OWNER
+key("n", "<leader>hl", [[:nohl <CR>]], nor)                            -- GET RID OF HIGHTLIGHTS
