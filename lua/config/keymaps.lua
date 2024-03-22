55501295c24505b7bcdcc4b9f6858dbd6f52c65e
Shalaw Fatah <shalaw.fatah@gmail.com>
2024-03-22 04:09:42 -0700

    added line toggler and keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,33 +1,41 @@
 -- LOCAL VARIABLES
 local key = vim.keymap.set
-local nor = { noremap = true, silent = true }
+local opts = { noremap = true, silent = true }
 local buf = ":lua vim.lsp.buf."
 -- TELESCOPE KEYBINDINGS
-key("n", "<C-p>", ":Telescope find_files<CR>", nor)                    -- SHOW FILES IN THE PROJECT
-key("n", "<leader>lg", ":Telescope live_grep<CR>", nor)                -- SEARCH PROJECT FOR WORDS
-key("n", "<leader>bf", ":Telescope buffers<CR>", nor)                  -- SHOW OPEN BUFFERS
-key("n", "<leader>h", ":Telescope help_tags<CR>", nor)                 -- SHOW HELP TAGS
-key("n", "<leader>u", ":Telescope undo<CR>", nor)                      -- SHOW UNDO TREE
-key("n", "<leader>w", ":Telescope projects<CR>", nor)                  -- SHOW PROJECTS
-key("n", "<leader>y", ":Telescope neoclip<CR>", nor)                   -- SHOW COPY-PASTE TREE
+key("n", "<C-p>", ":Telescope find_files<CR>", opts)                    -- SHOW FILES IN THE PROJECT
+key("n", "<leader>lg", ":Telescope live_grep<CR>", opts)                -- SEARCH PROJECT FOR WORDS
+key("n", "<leader>bf", ":Telescope buffers<CR>", opts)                  -- SHOW OPEN BUFFERS
+key("n", "<leader>h", ":Telescope help_tags<CR>", opts)                 -- SHOW HELP TAGS
+key("n", "<leader>u", ":Telescope undo<CR>", opts)                      -- SHOW UNDO TREE
+key("n", "<leader>w", ":Telescope projects<CR>", opts)                  -- SHOW PROJECTS
+key("n", "<leader>y", ":Telescope neoclip<CR>", opts)                   -- SHOW COPY-PASTE TREE
 -- LSP KEYBINDINGS
-key("n", "K", buf .. "hover()<CR>", nor)                               -- HOVER
-key("n", "gd", buf .. "definition()<CR>", nor)                         -- WHERE IS THE CODE DEFINED
-key("n", "gt", buf .. "type_definition()<CR>", nor)                    -- WHAT IS THE TYPE OF THE CODE
-key("n", "<leader>ft", buf .. "format() <CR>", nor)                    -- WHAT IS THE TYPE OF THE CODE
-key("n", "<leader>ca", buf .. "code_action()<CR>", nor)                -- WHICH CODE ACTION IS AVAILABLE
-key("n", "<leader>rf", ":Telescope lsp_references<CR>", nor)           -- WHERE IS THE CODE REFERENCED
-key("n", "<leader>rn", buf .. "rename()<CR>", nor)                     -- RENAME THE CODE THROUGHOUT ALL
-key("i", "<C-Enter>", buf .. "completion()<CR>", nor)                  -- WHAT COMES AS CODE COMPLETIOn
-key("n", "<leader>ds", buf .. "document_symbol()<CR>", nor)            -- DECLARED VARS, FUNCS, CLASSES
+key("n", "K", buf .. "hover()<CR>", opts)                               -- HOVER
+key("n", "gd", buf .. "definition()<CR>", opts)                         -- WHERE IS THE CODE DEFINED
+key("n", "gt", buf .. "type_definition()<CR>", opts)                    -- WHAT IS THE TYPE OF THE CODE
+key("n", "<leader>ft", buf .. "format() <CR>", opts)                    -- WHAT IS THE TYPE OF THE CODE
+key("n", "<leader>ca", buf .. "code_action()<CR>", opts)                -- WHICH CODE ACTION IS AVAILABLE
+key("n", "<leader>rf", ":Telescope lsp_references<CR>", opts)           -- WHERE IS THE CODE REFERENCED
+key("n", "<leader>rn", buf .. "rename()<CR>", opts)                     -- RENAME THE CODE THROUGHOUT ALL
+key("i", "<C-Enter>", buf .. "completion()<CR>", opts)                  -- WHAT COMES AS CODE COMPLETIOn
+key("n", "<leader>ds", buf .. "document_symbol()<CR>", opts)            -- DECLARED VARS, FUNCS, CLASSES
 -- COMMON LANGUAGE KEYBINDINGS
-key("n", "<leader>js", [[:!node %<CR>]], nor)                          -- RUN CURRENT JS FILE
-key("n", "<leader>ts", [[:!tsc %<CR>]], nor)                           -- RUN CURRENT TS FILE
-key("n", "<leader>go", [[:!go run %<CR>]], nor)                        -- RUN CURRENT GO FILE
+key("n", "<leader>js", [[:!node %<CR>]], opts)                          -- RUN CURRENT JS FILE
+key("n", "<leader>ts", [[:!tsc %<CR>]], opts)                           -- RUN CURRENT TS FILE
+key("n", "<leader>go", [[:!go run %<CR>]], opts)                        -- RUN CURRENT GO FILE
+-- LINE MOVER PLUGIN
+key("n", "<A-Down>", ":MoveLine 1<CR>", opts)                           -- MOVE LINE UP
+key("n", "<A-Up>", ":MoveLine -1<CR>", opts)                            -- MOVE LINE DOWN 
+key("x", "<A-Up>", ":MoveBlock -1<CR>", opts)                           -- MOVE BLOCK UP
+key("x", "<A-Down>", ":MoveBlock 1<CR>", opts)                          -- MOVE BLOCK DOWN
+key("n", "<A-Left>", ":MoveWord -1<CR>", opts)                          -- MOVE WORD LEFT 
+key("n", "<A-Right>", ":MoveWord 1<CR>", opts)                          -- MOVE WORD RIGHT
 -- UTILITY KEYS
-key({ "n", "v" }, "<leader>o", ":Oil<CR>", nor)                        -- FILE EDITING
-key({ "n", "v" }, "<C-s>", ":Silicon<CR>", nor)                        -- SCREENSHOT
-key({ "n", "v" }, "<leader>b", ":Neotree filesystem toggle<CR>", nor)  -- EXPLORER
-key("n", "<leader>md", ":MarkdownPreviewToggle<CR>", nor)              -- MARKDOWN TOGGLE
-key("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", nor) -- SHOW GIT LINE COMMIT OWNER
-key("n", "<leader>hl", [[:nohl <CR>]], nor)                            -- GET RID OF HIGHTLIGHTS
+key({ "n", "v" }, "<leader>o", ":Oil<CR>", opts)                        -- FILE EDITING
+key({ "n", "v" }, "<C-s>", ":Silicon<CR>", opts)                        -- SCREENSHOT
+key({ "n", "v" }, "<leader>b", ":Neotree filesystem toggle<CR>", opts)  -- EXPLORER
+key("n", "<leader>md", ":MarkdownPreviewToggle<CR>", opts)              -- MARKDOWN TOGGLE
+key("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", opts) -- SHOW GIT LINE COMMIT OWNER
+key("n", "<leader>hl", [[:nohl <CR>]], opts)                            -- GET RID OF HIGHTLIGHTS
+key("n", "<S-Up>", [[:lua require("togglr").toggle_word()<CR>]], opts)  -- TOGGLE WORD
---
+++ lua/plugins/word-toggler.lua
@@ -0,0 +1,11 @@
+return {
+  "elentok/togglr.nvim",
+  opts = {
+    register = "t",
+    debug = false,
+    values = {
+      ["good"] = "evil",
+      ["even"] = "odd",
+    },
+  },
+}
