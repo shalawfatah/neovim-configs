d1b56b921d6bfa8911c7839042735234da6a5b1b
Shalaw Fatah <shalaw.fatah@gmail.com>
2024-03-24 06:09:57 -0700

    catpuccin update
    catpuccin theme was not affecting the terminal, fixed.

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -6,7 +6,59 @@ return {
   priority = 1000,
   config = function()
     require("catppuccin").setup({
+      flavour = "mocha",
+      background = {
+        light = "latte",
+        dark = "mocha",
+      },
       transparent_background = true,
+      show_end_of_buffer = false,
+      term_colors = false,
+      dim_inactive = {
+        enabled = false,
+        shade = "dark",
+        percentage = 0.15,
+      },
+      no_italic = false,
+      no_bold = false,
+      no_underline = false,
+      styles = {
+        comments = { "italic" },
+        conditionals = {},
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+      },
+      color_overrides = {
+        mocha = {
+          base = "#2a2734",
+          mantle = "#2a2734",
+          crust = "#2a2734",
+        }
+      },
+      custom_highlights = {},
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = true,
+        leap = true,
+        mason = true,
+        neotree = true,
+        noice = true,
+        mini = {
+          enabled = true,
+          indentscope_color = "",
+        },
+      },
     })
     vim.cmd.colorscheme("catppuccin")
   end,
