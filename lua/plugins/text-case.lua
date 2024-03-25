1a89c8539445a7c60ee7326b69b2f858e156abb8
Shalaw Fatah <shalaw.fatah@gmail.com>
2024-03-25 01:12:39 -0700

    deleted text-case plugin
    text-case converter was deleted after it was found `~` does the job of
    uppercase to lowercase and vice versa, better.

--- lua/plugins/text-case.lua
+++
@@ -1,8 +0,0 @@
--- CONVERT TEXT CASES 
--- KEYBINDINGS: <leader>txg FOR ALL OPTIONS, <leader>txu FOR UPPERCASE, <leader>txl FOR LOWERCASE 
--- KEYBINDINGS: <leader>txs FOR SNAKE CASE, <leader>txd FOR DASH CASE 
-
-return {
-  "johmsalas/text-case.nvim",
-  config = true,
-}
