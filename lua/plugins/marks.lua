337523519f543fc2c3d9f3248fb4c5d0d1667bcb
Shalaw Fatah <shalaw.fatah@gmail.com>
2024-03-25 08:56:09 -0700

    mark comments on plugin
    Wrote comments for mark.nvim plugin

--- lua/plugins/marks.lua
+++ lua/plugins/marks.lua
@@ -1,3 +1,8 @@
+-- MANAGE MARKS 
+-- ma CREATES MARK a, mA CREATES MARK A (global), dma REMOVES MARK a
+-- m:a SHOWS MARK a IN TELESCOPE, m:W SHOWS MARK W IN TELESCOPE 
+-- dm<space> DELETE ALL MARKS IN CURRENT BUFFER
+
 return {
   "chentoast/marks.nvim",
   config = function()
