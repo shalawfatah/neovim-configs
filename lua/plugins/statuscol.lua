0b7233facf51bda9953c2e5ebaecc0024a548351
Shalaw Fatah <shalaw.fatah@gmail.com>
2024-03-28 12:09:28 -0700

    removed comments from status col
    Removed two line comments from status col plugin to make it look better

--- lua/plugins/statuscol.lua
+++ lua/plugins/statuscol.lua
@@ -6,8 +6,6 @@ return {
     local builtin = require("statuscol.builtin")
     return {
       setopt = true,
-      -- override the default list of segments with:
-      -- number-less fold indicator, then signs, then line number & separator
       segments = {
         { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
         { text = { "%s" }, click = "v:lua.ScSa" },
