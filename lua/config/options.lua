7c8abc608d809c9c66e4277fe4e0d3c53b515292
Shalaw Fatah <shalaw.fatah@gmail.com>
2024-03-28 13:25:31 -0700

    removed relative number option
    Removed relative number as it's not used a lot

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -10,7 +10,6 @@ opt.softtabstop = 2
 opt.shiftwidth = 2
 opt.clipboard = "unnamedplus"
 opt.number = true
-opt.relativenumber = true
 vim.g.mapleader = " "
 vim.o.signcolumn = "yes"
 opt.foldcolumn = '1'
