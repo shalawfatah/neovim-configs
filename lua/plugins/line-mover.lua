a2465bc4cfe1fe48242b6809ffbcf1adceba83d4
Shalaw Fatah <shalaw.fatah@gmail.com>
2024-04-03 00:35:58 -0700

    formatting issue on line mover
    formatting issue with line mover plugin.

--- lua/plugins/line-mover.lua
+++ lua/plugins/line-mover.lua
@@ -1,4 +1,4 @@
--- MOVES LINES, BLOCKS OF CODE UP AND DOWN 
+-- MOVES LINES, BLOCKS OF CODE UP AND DOWN
 -- LINE [normal mode] alt + Up/Down ARROW, BLOCK [visual mode] alt + Up/Down ARROW
 
 return {
