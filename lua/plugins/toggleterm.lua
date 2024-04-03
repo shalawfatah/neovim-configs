c990718a5a72f3af74fda84aa9450ef4ae23231c
Shalaw Fatah <shalaw.fatah@gmail.com>
2024-04-03 08:17:14 -0700

    spelling correction, toggle

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,4 +1,4 @@
--- OPEN TERMIANLS IN FLOAT: Ctrl + \, HORIZONTAL: Ctrl + h AND VERTICAL: Ctrl + v WAYS 
+-- OPEN TERMIANLS IN FLOAT: Ctrl + /, HORIZONTAL: Ctrl + h AND VERTICAL: Ctrl + v WAYS 
 
 return  {
   "akinsho/toggleterm.nvim",
