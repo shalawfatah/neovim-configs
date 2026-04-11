6004a881bda39012edda11285048da195345e541
Shalaw Fatah <shalaw.fatah@gmail.com>
2026-04-11 22:35:45 +0300

    updated instructions on flash.nvim

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,15 +1,18 @@
+-- NAVIGATE BUFFER WITH SEARCH LABELS
+--  HIT s THEN CHAR YOU LOOKING FOR, THEN CHAR THAT APPEARS
+
 return {
-  "folke/flash.nvim",
-  event = "VeryLazy",
-  opts = {},
-  keys = {
-    {
-      "s",
-      mode = { "n", "x", "o" },
-      function()
-        require("flash").jump()
-      end,
-      desc = "Flash",
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+        {
+            "s",
+            mode = { "n", "x", "o" },
+            function()
+                require("flash").jump()
+            end,
+            desc = "Flash",
+        },
     },
-  },
 }
