fc54aedf8370682dfb0a7744deb16003b061c617
Shalaw Fatah <shalaw.fatah@gmail.com>
2026-04-11 22:23:55 +0300

    better comment on word toggle plugin

--- lua/plugins/word-toggler.lua
+++ lua/plugins/word-toggler.lua
@@ -1,14 +1,14 @@
 -- TOGGLE COMMON WORDS, LIKE true, false
--- WORKS WITH KEYBINDING SHIFT + Up ARROW
+-- WORKS WITH KEYBINDING SHIFT + UP/DOWN ARROW
 
 local toggledWords = require("utils.toggled_words")
 
 return {
-  "elentok/togglr.nvim",
-  event = "VeryLazy",
-  opts = {
-    register = "t",
-    debug = false,
-    values = toggledWords,
-  },
+    "elentok/togglr.nvim",
+    event = "VeryLazy",
+    opts = {
+        register = "t",
+        debug = false,
+        values = toggledWords,
+    },
 }
