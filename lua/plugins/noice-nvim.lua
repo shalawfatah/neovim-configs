c853d42a11df20b384cb78625d0702db5a2b01ce
Shalaw Fatah <shalaw.fatah@gmail.com>
2026-04-11 22:38:45 +0300

    plugins updated

--- lua/plugins/noice-nvim.lua
+++ lua/plugins/noice-nvim.lua
@@ -1,40 +1,41 @@
 -- BETTER NOTIFICATION SYSTEM
 
 return {
-  {
-    "rcarriga/nvim-notify",
-    event = "VeryLazy",
-    config = function()
-      require("notify").setup({
-        background_colour = "#000000",
-      })
-    end,
-  },
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {},
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
+    {
+        "rcarriga/nvim-notify",
+        event = "VeryLazy",
+        config = function()
+            require("notify").setup({
+                background_colour = "#000000",
+                merge_duplicates = true,
+            })
+        end,
     },
-    config = function()
-      require("noice").setup({
-        lsp = {
-          override = {
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            ["vim.lsp.util.stylize_markdown"] = true,
-            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-          },
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {},
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "rcarriga/nvim-notify",
         },
-        presets = {
-          bottom_search = true, -- use a classic bottom cmdline for search
-          command_palette = true, -- position the cmdline and popupmenu together
-          long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = false, -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = false, -- add a border to hover docs and signature help
-        },
-      })
-    end,
-  },
+        config = function()
+            require("noice").setup({
+                lsp = {
+                    override = {
+                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                        ["vim.lsp.util.stylize_markdown"] = true,
+                        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+                    },
+                },
+                presets = {
+                    bottom_search = true, -- use a classic bottom cmdline for search
+                    command_palette = true, -- position the cmdline and popupmenu together
+                    long_message_to_split = true, -- long messages will be sent to a split
+                    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+                    lsp_doc_border = false, -- add a border to hover docs and signature help
+                },
+            })
+        end,
+    },
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,32 +3,32 @@
 local opts = { noremap = true, silent = true, nowait = true }
 
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    branch = "0.1.x",
-    event = "VeryLazy",
-    keys = {
-      { "<C-p>",      ":Telescope find_files<CR>",     opts, mode = "n" }, -- SHOW DIR & FILES IN THE PROJECT
-      { "<leader>lg", ":Telescope live_grep<CR>",      opts, mode = "n" }, -- LIVE SEARCH IN ENTIRE PROJECT
-      { "<leader>fw", ":Telescope grep_string<CR>",    opts, mode = "n" }, -- FIND WORD
-      { "<leader>f",  ":Telescope buffers<CR>",        opts, mode = "n" }, -- SHOW OPEN BUFFERS
-      { "<leader>h",  ":Telescope help_tags<CR>",      opts, mode = "n" }, -- SHOW HELP TAGS
-      { "<leader>u",  ":Telescope undo<CR>",           opts, mode = "n" }, -- SHOW UNDO TREE
-      { "<leader>w",  ":Telescope projects<CR>",       opts, mode = "n" }, -- SHOW PROJECTS
-      { "<leader>y",  ":Telescope neoclip<CR>",        opts, mode = "n" }, -- SHOW YANK TREE
-      { "<leader>ld", ":Telescope diagnostics<CR>",    opts, mode = "n" }, -- SHOW ALL LINTING ERRORS
-      { "<leader>rf", ":Telescope lsp_references<CR>", opts, mode = "n" }, -- WHERE IS THE CODE REFERENCED
+    {
+        "nvim-telescope/telescope.nvim",
+        branch = "0.1.x",
+        event = "VeryLazy",
+        keys = {
+            { "<C-p>",      ":Telescope find_files<CR>",     opts, mode = "n" }, -- SHOW DIR & FILES IN THE PROJECT
+            { "<leader>lg", ":Telescope live_grep<CR>",      opts, mode = "n" }, -- LIVE SEARCH IN ENTIRE PROJECT
+            { "<leader>fw", ":Telescope grep_string<CR>",    opts, mode = "n" }, -- FIND WORD
+            { "<leader>f",  ":Telescope buffers<CR>",        opts, mode = "n" }, -- SHOW OPEN BUFFERS
+            { "<leader>h",  ":Telescope help_tags<CR>",      opts, mode = "n" }, -- SHOW HELP TAGS
+            { "<leader>u",  ":Telescope undo<CR>",           opts, mode = "n" }, -- SHOW UNDO TREE
+            { "<leader>w",  ":Telescope projects<CR>",       opts, mode = "n" }, -- SHOW PROJECTS
+            { "<leader>y",  ":Telescope neoclip<CR>",        opts, mode = "n" }, -- SHOW YANK TREE
+            { "<leader>ld", ":Telescope diagnostics<CR>",    opts, mode = "n" }, -- SHOW ALL LINTING ERRORS
+            { "<leader>rf", ":Telescope lsp_references<CR>", opts, mode = "n" }, -- WHERE IS THE CODE REFERENCED
+        },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            {
+                "nvim-telescope/telescope-fzf-native.nvim",
+                build =
+                "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+            },
+        },
+        config = function()
+            require("telescope").setup({})
+        end,
     },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build =
-        "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
-      },
-    },
-    config = function()
-      require("telescope").setup({})
-    end,
-  },
 }
