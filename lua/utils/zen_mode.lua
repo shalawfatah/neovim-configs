ce1805cd0e7d6b975dbdea4eab70717c8bc98ad3
Shalaw Fatah <shalaw.fatah@gmail.com>
2026-04-12 04:12:57 +0300

    fixed zen mode

--- lua/utils/zen_mode.lua
+++ lua/utils/zen_mode.lua
@@ -4,15 +4,11 @@ local M = {}
 
 function M.ToggleLastStatus()
   if vim.o.laststatus == 0 then
-    vim.api.nvim_set_option('laststatus', 2)
-    -- vim.o.signcolumn = "yes"
-    -- vim.opt.foldcolumn = '1'
-    vim.api.nvim_command("Twilight")
+    vim.o.laststatus = 2
+    vim.cmd("Twilight")
   else
-    vim.api.nvim_set_option('laststatus', 0)
-    -- vim.o.signcolumn = "no"
-    -- vim.opt.foldcolumn = '0'
-    vim.api.nvim_command("Twilight")
+    vim.o.laststatus = 0
+    vim.cmd("Twilight")
   end
 end
 
