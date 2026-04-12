4d53edc8ad7738e7b203265de78e6b8427320c6a
Shalaw Fatah <shalaw.fatah@gmail.com>
2026-04-12 04:06:11 +0300

    added highlight yanked text briefly and restore cursor

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,11 +1,20 @@
--- Autocmds are automatically loaded on the VeryLazy event
+-- autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "python",
+-- Highlight yanked text briefly
+vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
-    vim.opt_local.tabstop = 4
-    vim.opt_local.shiftwidth = 4
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 150 })
+  end,
+})
+
+-- Restore cursor position when reopening a file
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    if mark[1] > 1 and mark[1] <= vim.api.nvim_buf_line_count(0) then
+      vim.api.nvim_win_set_cursor(0, mark)
+    end
   end,
 })
