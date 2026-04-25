7f2cfe781b6fecd1aecff94085dadcda206f1ba3
Shalaw Fatah <shalaw.fatah@gmail.com>
2026-04-25 11:05:51 +0300

    md keybinding added

--- lua/plugins/peek.lua
+++ lua/plugins/peek.lua
@@ -6,5 +6,7 @@ return {
     require("peek").setup()
     vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
     vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    vim.keymap.set("n", "<leader>md", "<cmd>PeekOpen<CR>", { desc = "Markdown Preview Open" })
+    vim.keymap.set("n", "<leader>mx", "<cmd>PeekClose<CR>", { desc = "Markdown Preview Close" })
   end,
 }
